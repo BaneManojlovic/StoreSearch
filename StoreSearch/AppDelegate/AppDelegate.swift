8176e942666ec2c3f593ac1023e0330c25da1321
Bane Manojlovic <bane1manojlovic@gmail.com>
2020-07-30 14:48:22 +0200

    Added App icon & set selected background color for SearchResultCell

--- StoreSearch/AppDelegate/AppDelegate.swift
+++ StoreSearch/AppDelegate/AppDelegate.swift
@@ -15,8 +15,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        customizeAppearance()
         return true
     }
 
+    // MARK: - Helper Methods
+    func customizeAppearance() {
+        let barTintColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
+        UISearchBar.appearance().barTintColor = barTintColor
+        window!.tintColor = UIColor(red: 10/255, green: 80/255, blue: 80/255, alpha: 1)
+        
+        // For setting white background for searchbar textfield in case of ios13 OS
+        if #available(iOS 13.0, *) {
+            UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = .white
+            // UISearchBar.appearance().searchTextField.backgroundColor = UIColor.white
+        }
+    }
 }
 
