8d0c1dedcac035295bba2c933b1de928114bcd44
Bane Manojlovic <bane1manojlovic@gmail.com>
2020-07-30 10:57:49 +0200

    Added SearchResult model and it's implementation

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Bane Manojlovic on 30/07/2020.
+//  Copyright © 2020 Bane Manojlovic. All rights reserved.
+//
+
+import UIKit
+
+class SearchResult {
+    
+    // MARK: - Properties
+    var name = ""
+    var artistName = ""
+    
+}
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -15,14 +15,25 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     // MARK: - Properties
-    
+    var searchResults = [SearchResult]()
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+        setupUI()
+        setupDelegates()
+    }
+    
+    func setupUI() {
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+    }
+    
+    func setupDelegates() {
+        searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
@@ -30,5 +41,58 @@ class SearchViewController: UIViewController {
 
 // MARK: - Conforming to UISearchBarDelegate procotol
 extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("The text is: \(searchBar.text!)")
+        
+        searchResults = []
+        
+        for i in 0...2 {
+            
+            let searchResult = SearchResult()
+            searchResult.name = String(format: "Fake result %d for", i)
+            searchResult.artistName = searchBar.text!
+            
+            searchResults.append(searchResult)
+        }
+        
+        
+        tableView.reloadData()
+        searchBar.resignFirstResponder()
+    }
+    
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
+
+
+// MARK: - Conforming to UITableViewDataSource
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        }
+        
+        let searchResult = searchResults[indexPath.row]
+        cell.textLabel!.text = searchResult.name
+        cell.detailTextLabel!.text = searchResult.artistName
+        
+        return cell
+    }
+    
+    
+}
+
+// MARK: - Conforming to UITableViewDelegate
+extension SearchViewController: UITableViewDelegate {
     
 }
