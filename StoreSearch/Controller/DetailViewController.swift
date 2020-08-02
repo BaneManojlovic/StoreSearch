e7887c52ad243614664f580f6c8955a301eb8f62
Bane Manojlovic <bane1manojlovic@gmail.com>
2020-08-02 14:13:24 +0200

    Finished layout for DetailViewController pop-up screen

--- StoreSearch/Controller/DetailViewController.swift
+++ StoreSearch/Controller/DetailViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var kindLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var priceButton: UIButton!
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+        setupTargets()
 
     }
     
@@ -20,6 +32,19 @@ class DetailViewController: UIViewController {
         super.init(coder: aDecoder)
         modalPresentationStyle = .custom
         transitioningDelegate = self
+    }
+    
+    // MARK: - Setup Methods
+    func setupUI() {
+        view.tintColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
+        popupView.layer.cornerRadius = 10
+    }
+    
+    func setupTargets() {
+        let gestureReckognizer = UITapGestureRecognizer(target: self, action: #selector(close))
+        gestureReckognizer.cancelsTouchesInView = false
+        gestureReckognizer.delegate = self
+        view.addGestureRecognizer(gestureReckognizer)
     }
     
     // MARK: - Action Methods
@@ -34,5 +59,13 @@ extension DetailViewController: UIViewControllerTransitioningDelegate {
     
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         return DimmingPresentationController(presentedViewController: presented, presenting: presenting)
+    }
+}
+
+// MARK: - Conforming to UIGestureReckognizerDelegate
+extension DetailViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return (touch.view === self.view)
     }
 }
