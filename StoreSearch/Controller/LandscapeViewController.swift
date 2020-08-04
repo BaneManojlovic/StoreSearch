aee7611758d7904c6803b5381bffdd9f9a927548
Bane Manojlovic <bane1manojlovic@gmail.com>
2020-08-04 15:36:00 +0200

    Added page control feature on LandscapeViewController

--- StoreSearch/Controller/LandscapeViewController.swift
+++ StoreSearch/Controller/LandscapeViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class LandscapeViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet  weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    func setupUI() {
+        // Remove Constraints from the main view
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        
+        // Remove Constraints for page control
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        
+        // Remove Constraints for scrollView
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+    }
+    
+    // MARK: - Pverriden Methods
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        let safeFrame = view.safeAreaLayoutGuide.layoutFrame
+        scrollView.frame = safeFrame
+        
+        pageControl.frame = CGRect(x: safeFrame.origin.x,
+                                   y: safeFrame.size.height - pageControl.frame.size.height,
+                                   width: safeFrame.size.width,
+                                   height: pageControl.frame.size.height)
     }
 
 }
